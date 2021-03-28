dd80815cf9d5d5d0c437fe2751f18b63c9d84fd3
AlejandroBohal <sergio.bohorquez@mail.escuelaing.edu.co>
2021-03-28 12:08:22 -0500

    Test using junit.jupiter instead junit

---
+++ src/test/java/edu/escuelaing/ieti/startapp/web/ProjectControllerTests.java
@@ -0,0 +1,38 @@
+package edu.escuelaing.ieti.startapp.web;
+
+import edu.escuelaing.ieti.startapp.business.model.Finance;
+import edu.escuelaing.ieti.startapp.business.model.Project;
+import edu.escuelaing.ieti.startapp.business.services.projectservices.IProjectServices;
+import edu.escuelaing.ieti.startapp.business.services.projectservices.impl.ProjectServicesImpl;
+import edu.escuelaing.ieti.startapp.web.controllers.ProjectController;
+import edu.escuelaing.ieti.startapp.web.requests.ProjectRequest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.mockito.Mockito;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Date;
+
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+
+public class ProjectControllerTests {
+
+    private IProjectServices projectServicesMock = Mockito.mock(ProjectServicesImpl.class);
+    private ProjectController projectController = new ProjectController(projectServicesMock);
+
+    @Test
+    void shouldCreateProject() {
+        Finance financeTest = new Finance(1L,2,1L,2L,new Date(),new Date());
+        Project testProject = new Project("test", "abc.com", "abc.com", "CO", "testDesc",financeTest);
+        when(projectServicesMock.createProject(Mockito.any())).thenReturn(testProject);
+        ResponseEntity<Object> httpResponse = projectController.createProject(new ProjectRequest(testProject));
+        Assertions.assertEquals(httpResponse.getStatusCode(), HttpStatus.CREATED);
+    }
+
+}
