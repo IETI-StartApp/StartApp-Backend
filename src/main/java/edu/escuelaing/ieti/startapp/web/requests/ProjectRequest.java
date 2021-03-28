b0adda12901bd99395ee08b7c6edf3ad3f25795a
AlejandroBohal <sergio.bohorquez@mail.escuelaing.edu.co>
2021-03-28 12:37:22 -0500

    Fixed code smells

--- src/main/java/edu/escuelaing/ieti/startapp/web/requests/ProjectRequest.java
+++ src/main/java/edu/escuelaing/ieti/startapp/web/requests/ProjectRequest.java
@@ -2,9 +2,6 @@ package edu.escuelaing.ieti.startapp.web.requests;
 
 import edu.escuelaing.ieti.startapp.business.model.Finance;
 import edu.escuelaing.ieti.startapp.business.model.Project;
-import lombok.Getter;
-import lombok.Setter;
-import org.springframework.data.annotation.Id;
 
 import java.io.Serializable;
 
@@ -32,59 +29,5 @@ public class ProjectRequest implements Serializable {
         return new Project(name,image,video,country,description,finance);
     }
 
-    public String getId() {
-        return id;
-    }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
-
-    public String getVideo() {
-        return video;
-    }
-
-    public void setVideo(String video) {
-        this.video = video;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Finance getFinance() {
-        return finance;
-    }
-
-    public void setFinance(Finance finance) {
-        this.finance = finance;
-    }
 }
